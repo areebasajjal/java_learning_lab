565d5e00ea3166125f9438373edad4c7c305a514
areebasajjal <areebasajjal22@gmail.com>
2026-07-22 22:43:19 +0500

    feat: stataic vs instance methods and variables

--- learn1OOP.java
+++ learn1OOP.java
@@ -2,13 +2,30 @@ class student {
     String name;
     int age;
     int rollno;
+    static int count = 0;
+    static String university = "COMSATS";
+
+
+    void incrementCount() {
+        count++;
+    }
     
     void studies() {
-        System.out.println(name + "is studying");
+        System.out.println(name + " is studying");
     }
 
     void attendsSchool() {
-        System.out.println(name + "is attending school");
+        System.out.println(name + " is attending school");
+    }
+
+    static void university(){
+        System.out.println("student belongs to " + university);
+    } // static methods have no this hence they can only work with static variables.
+
+    void display(){
+        System.out.println(name + " is studying at " + university); 
+        // inwardly, its this.name and student.university 
+
     }
 
 }
@@ -22,5 +39,11 @@ public class learn1OOP {
     // student s1; declation created reference variable 
     // new keyword creates the actual object. 
     // wherever we use the new keyword, a new object is created in the heap memory 
+
+    System.out.println(student.count); // static variables can either be called by class name or the name of object 
+    // ( class name is more appropriate)
+    student.university();
+
+    s1.display();
     }
 }
